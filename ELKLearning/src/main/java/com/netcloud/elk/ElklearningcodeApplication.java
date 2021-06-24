1c645ffe0ddf6501c21b5678f1114f9d5157376b
yangshaojun <yangsj@hitechairi.com>
2021-06-24 19:01:13 +0800

    提交ELK项目

---
+++ ELKLearning/src/main/java/com/netcloud/elk/ElklearningcodeApplication.java
@@ -0,0 +1,13 @@
+package com.netcloud.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElklearningcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElklearningcodeApplication.class, args);
+    }
+
+}
---
+++ ELKLearning/src/test/java/com/netcloud/elk/ElklearningcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcloud.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElklearningcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
